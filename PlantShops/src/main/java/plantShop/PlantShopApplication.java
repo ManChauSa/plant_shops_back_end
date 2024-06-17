62ae58cdf5746df4f81fc11dd8c62e1cedf9f6a8
XienXien <tiennguyentpm3@gmail.com>
2024-06-17 14:23:53 -0500

    add src

---
+++ PlantShops/src/main/java/plantShop/PlantShopApplication.java
@@ -0,0 +1,19 @@
+package plantShop;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class PlantShopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PlantShopApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper()
+    {
+        return new ModelMapper();
+    }
+}
